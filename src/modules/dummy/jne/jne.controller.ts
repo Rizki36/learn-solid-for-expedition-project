728ef45da1b0379b166315bb11caba97a9720d69
Rizki36 <cahrizki.rf@gmail.com>
2022-05-10 15:25:26 +0700

    refactor: change response create order jne

--- src/modules/dummy/jne/jne.controller.ts
+++ src/modules/dummy/jne/jne.controller.ts
@@ -1,18 +1,17 @@
 import { NextFunction, Request, Response } from "express";
 import type { IResponseCreateOrderJNE } from "../../../dto/createOrder/jne";
-import { successResponse } from "../../../helpers/methods";
 import { VendorController } from "../vendor.controller";
 
 export default class JneController extends VendorController {
     createOrder(req: Request, res: Response, next: NextFunction) {
         try {
             const data: IResponseCreateOrderJNE = {
-                detail: [{ status: "success", cnote_no: req.body.awb }],
+                detail: [{ status: "success", cnote_no: req.body.AWB }],
             };
 
             // delay for 1 seconds
             setTimeout(() => {
-                successResponse(res, { data });
+                res.send(data);
             }, 1000);
         } catch (error) {
             next(error);
